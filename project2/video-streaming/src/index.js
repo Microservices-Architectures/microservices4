5e80617bcf3fb2a6d603100f5ebe7bf1986614f0
chi <chinedu.onyeoka@gmail.com>
2021-08-03 16:25:26 +0100

    server for project 2 setup

--- project2/video-streaming/src/index.js
+++ project2/video-streaming/src/index.js
@@ -22,3 +22,35 @@ if(!process.env.VIDEO_STORAGE_PORT) {
 //
 // Extracts environment variables to globals for convenience.
 //
+
+const PORT = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
+
+//
+// Registers a HTTP GET route for video streaming.
+//
+
+app.get("/video", (req, res) => {
+    const forwardRequest = http.request(// we are forwarding the request to the video storage microservice
+        {
+            host: VIDEO_STORAGE_HOST,
+            port: VIDEO_STORAGE_PORT,
+            path: '/video?path=talent.mp4', // hard coded video path  
+            method: 'GET',
+            headers: req.headers
+        },
+        forwardResponse => {
+            res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+            forwardResponse.pipe(res);
+        }
+        )
+        req.pipe(forwardRequest)
+})
+//
+// Starts the HTTP server.
+//
+app.listen(PORT, () => {
+    console.log(`Microservice online`);
+});

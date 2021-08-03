ec14dd454721b32d4b8a2ede2b78acd6da7c8a64
chi <chinedu.onyeoka@gmail.com>
2021-08-03 16:08:08 +0100

    wip

--- project2/video-streaming/src/index.js
+++ project2/video-streaming/src/index.js
@@ -1,40 +1,24 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
 
-const app = express();
-const port = 3000;
-
-//
-// Registers a HTTP GET route for video streaming.
-//
-// Original code for this:
-// https://medium.com/better-programming/video-stream-with-node-js-and-html5-320b3191a6b6
-//
-app.get("/video", (req, res) => {
-
-    //
-    // Original video from here:
-    // https://sample-videos.com
-    //
-    const path = "../videos/SampleVideo_1280x720_1mb.mp4";
-    fs.stat(path, (err, stats) => {
-        if (err) {
-            console.error("An error occurred ");
-            res.sendStatus(500);
-            return;
-        }
-
-        res.writeHead(200, {
-            "Content-Length": stats.size,
-            "Content-Type": "video/mp4",
-        });
-        fs.createReadStream(path).pipe(res);
-    });
-});
+const http = require('http');
+const app   = express();
 
 //
-// Starts the HTTP server.
+// Throws an error if the any required environment variables are missing.
 //
-app.listen(port, () => {
-    console.log(`Microservice listening on port ${port}, point your browser at http://localhost:3000/video`);
-});
+if(!process.env.PORT) {
+    throw new Error("Please specify the port number for the http server with the environment variable PORT.");
+}
+
+if(!process.env.VIDEO_STORAGE_HOST) {
+    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
+}
+
+if(!process.env.VIDEO_STORAGE_PORT) {
+    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
+}
+
+
+//
+// Extracts environment variables to globals for convenience.
+//
